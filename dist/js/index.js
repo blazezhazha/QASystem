21531c56ee5e1e685fcf0b3aa06c31a6d7cd2216
blazezhazha <zjc19940216@163.com>
2016-07-20 00:09:56 +0800

    connect server

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,4 +1,33 @@
-function clk{
-    var question = document.getElementsById("input");
-    $.
+function clk(){
+    var question = document.getElementById("input").value;
+    var query = "p=" + encodeURIComponent(question) + "&jsoncallback=?";
+    var xmlhttp;
+    var htmlContent;
+
+    //console.log(question);
+    //console.log(query);
+
+    if(window.XMLHttpRequest){
+        xmlhttp = new XMLHttpRequest();
+    }
+    else{
+        xmlhttp = new ActiveObject("Microsoft.XMLHTTP");
+    }
+
+    htmlContent = "<div class='msg-container user_a'><div class='msg'>" + question + "</div></div>";
+    $(".dlg").append(htmlContent);
+
+    //console.log(htmlContent);
+
+    xmlhttp.open("GET", "http://219.228.57.75:18888/proxy?" + query, true);
+    xmlhttp.send();
+    xmlhttp.onreadystatechange = function(){
+        if(xmlhttp.readyState == 4 && xmlhttp.status == 200){
+            var answer = xmlhttp.responseText.split("</br>");
+            console.log(answer[1]);
+            var firstAnswer = answer[1].substring(2);
+            htmlContent = "<div class='msg-container user_b'><div class='msg'>" + firstAnswer + "</div></div>";
+            $(".dlg").append(htmlContent);
+        }
+    }
 }
